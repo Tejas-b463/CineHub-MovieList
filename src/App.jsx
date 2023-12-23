f89075223a5973c638602edca021d9722dbc34a0
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-12-23 12:54:25 +0530

    Api Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-
-
+import { useEffect } from "react";
+import {fetchDataFromApi} from "./utils/api"
 
 function App() {
+
+    useEffect(()=>{
+           ApiTest()
+        },[])
+   const ApiTest = () => {
+    fetchDataFromApi('/movie/popular')
+    .then((res)=>{
+      console.log(res);
+    })
+   }
+
   return (
     <div>
     CineHub
